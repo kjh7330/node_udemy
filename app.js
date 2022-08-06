da7d4c70276027be9a41ca37ced4717b76981130
kjh7330 <rlawngks007@gmail.com>
2022-08-06 23:49:19 +0900

    노드 서버 생성

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //proccess.exit();
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>Hello My Node.js server</h1></body>');
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.write('<html><head><title>노드서버</title><head>'); // response 에 데이터 기록
+        res.write('<body>');
+        res.write('<form action = "/message" method = "POST"><input type="text" name = "message"><button type = "submit">전송</button></form>')
+        res.write('</body>')
+        res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.write('<html><head><title>노드서버</title><head>'); // response 에 데이터 기록
+    res.write('<body>');
+    res.write('<h1>Hellow World</body>')
+    res.write('</body>');
     res.write('</html>');
     res.end();
 });
